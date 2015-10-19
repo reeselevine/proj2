696045fbf1e3590481d06c0944ac0af722635570
Reese Levine <reeselevine@yahoo.com>
2015-10-19 12:11:08 +1100

    Fix pitch issue and implement reverse movement
    Still can't get ghosts to be translucent. I think this is the version
    to submit now and then if we want to do textures do that later this week.
    Same with a better model for ghosts.

--- Ghost.cs
+++ Ghost.cs
@@ -22,7 +22,7 @@ namespace Project
         public Ghost(GameController game)
         {
             this.game = game;
-            color = new Color(new Vector3(255,255,255), 0.25f);
+            color = new Color(new Vector3(255, 255, 255), 0.1f);
             size = 2;
             height = 0.5f;
             seek = 0;
